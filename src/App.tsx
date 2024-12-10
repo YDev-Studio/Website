677807aa0f18508b93e8e562dad3e91f88c35f88
Siraphop Sukchu <gamesiraphop4826@gmail.com>
2024-12-10 22:58:04 +0700

    Some changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,10 @@ import './App.css'
 function App() {
   return (
     <>
+      <div className="container">
+        <h1 className="gradient">Tournament</h1>
+        <h2 className="gradient">Soon...</h2>
+      </div>
     </>
   )
 }
