c78227f6f33435825e5368a67fc8d374bf06b531
Siraphop Sukchu <gamesiraphop4826@gmail.com>
2024-12-14 22:08:18 +0700

    Fix UI

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import './App.css'
 function App() {
   return (
     <>
-      <div className="container">
+
+      <div className="flex flex-col items-center justify-center h-screen">
         <h1 className="gradient">Tournament</h1>
         <h2 className="gradient">Soon...</h2>
       </div>
